cc78f194c529602e8fb10c281492a4a986ea3e82
Ascendus <ascendus.dev@gmail.com>
2022-10-10 18:02:44 +1300

    chore: deployment

--- _next/static/axTMVxcDnrnJ74G4hjBcG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/961-d4633df168081d52.js","static/css/ef391eb75cd947a9.css","static/chunks/pages/index-85126e660b6f34fc.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
